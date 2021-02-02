87f005a85f0f60a0bb7a860133b532b8264cf080
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2021-02-02 15:36:28 +0500

    feat: add clear and draw methods

---
+++ src/engine/graphics.ts
@@ -0,0 +1,11 @@
+import { CompiledProgram } from './programs/program'
+import { PreparedModel } from './models/model'
+
+export function clear (gl: WebGLRenderingContext) {
+  gl.clearColor(0.8, 0.8, 0.8, 1.0)
+  gl.clear(gl.COLOR_BUFFER_BIT)
+}
+
+export function draw (gl: WebGLRenderingContext, program: CompiledProgram, model: PreparedModel) {
+  program.draw(gl, model)
+}
