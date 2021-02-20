cd0a547f3f30df08b49941f9b0331aa51d891f7e
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2021-02-20 23:15:29 +0500

    refactor: change vector math to be based on classes instead of functions

--- src/math/vector.ts
+++ src/math/vector.ts
@@ -1,226 +1,266 @@
-// Interfaces
+/**
+ * 2D vector.
+ */
+export class Vector2 {
+  public constructor (public x: number, public y: number) {}
 
-interface Vector {
-  readonly values: number[]
-}
-
-export interface Vector2 extends Vector {
-  readonly x: number
-  readonly y: number
-  readonly r: number
-
-  readonly g: number
-  readonly s: number
-  readonly t: number
-}
-
-export interface Vector3 extends Vector, Vector2 {
-  readonly z: number
-  readonly b: number
-  readonly p: number
-}
-
-export interface Vector4 extends Vector, Vector2, Vector3 {
-  readonly w: number
-  readonly a: number
-  readonly q: number
-}
-
-// Implementation
-
-class VectorImpl implements Vector, Vector2, Vector3, Vector4 {
-  public constructor (public readonly values: number[]) {}
-
-  get x (): number {
-    return this.values[0] ?? 0
+  /**
+   * Creates a copy of the vector.
+   */
+  clone (): Vector2 {
+    return new Vector2(this.x, this.y)
   }
 
-  get r (): number {
-    return this.values[0] ?? 0
+  /**
+   * Returns squared vector size (squared length).
+   */
+  sizeSquared (): number {
+    return this.x ** 2 + this.y ** 2
   }
 
-  get s (): number {
-    return this.values[0] ?? 0
+  /**
+   * Returns vector size (length).
+   */
+  size (): number {
+    return Math.sqrt(this.sizeSquared())
   }
 
-  get y (): number {
-    return this.values[1] ?? 0
+  /**
+   * Returns a normalized copy of the vector.
+   */
+  normalize (): Vector2 {
+    const size = this.size()
+    return new Vector2(this.x / size, this.y / size)
   }
 
-  get g (): number {
-    return this.values[1] ?? 0
+  /**
+   * Returns a sum of vector components.
+   */
+  sum (): number {
+    return this.x + this.y
   }
 
-  get t (): number {
-    return this.values[1] ?? 0
+  /**
+   * Returns a new vector representing a sum of two vectors.
+   * @param v Vector to add.
+   */
+  add (v: Vector2): Vector2 {
+    return new Vector2(this.x + v.x, this.y + v.y)
   }
 
-  get z (): number {
-    return this.values[2] ?? 0
+  /**
+   * Returns a new vector representing a difference between two vectors.
+   * @param v Vector to subtract.
+   */
+  subtract (v: Vector2): Vector2 {
+    return new Vector2(this.x - v.x, this.y - v.y)
   }
 
-  get b (): number {
-    return this.values[2] ?? 0
+  /**
+   * Returns a new vector where each component is a product of
+   * corresponding components in original vectors.
+   * @param v Second vector.
+   */
+  multiply (v: Vector2): Vector2 {
+    return new Vector2(this.x * v.x, this.y * v.y)
   }
 
-  get p (): number {
-    return this.values[2] ?? 0
+  /**
+   * Returns a new vector where each component is a ratio of
+   * corresponding components in original vectors.
+   * @param v Second vector.
+   */
+  divide (v: Vector2): Vector2 {
+    return new Vector2(this.x / v.x, this.y / v.y)
   }
 
-  get w (): number {
-    return this.values[3] ?? 0
-  }
-
-  get a (): number {
-    return this.values[3] ?? 0
-  }
-
-  get q (): number {
-    return this.values[3] ?? 0
+  /**
+   * Returns a dot product of two vectors.
+   * @param v Second vector.
+   */
+  dot (v: Vector2): number {
+    return this.x * v.x + this.y * v.y
   }
 }
 
-// Creation
+/**
+ * 3D vector.
+ */
+export class Vector3 {
+  public constructor (public x: number, public y: number, public z: number) {}
 
-export function vec2 (x: number, y: number): Vector2 {
-  return new VectorImpl([x, y])
-}
-
-export function vec3 (x: number, y: number, z: number): Vector3 {
-  return new VectorImpl([x, y, z])
-}
-
-export function vec4 (x: number, y: number, z: number, w: number): Vector4 {
-  return new VectorImpl([x, y, z, w])
-}
-
-// From number
-
-export function num2 (value: number): Vector2 {
-  return vec2(value, value)
-}
-
-export function num3 (value: number): Vector3 {
-  return vec3(value, value, value)
-}
-
-export function num4 (value: number): Vector4 {
-  return vec4(value, value, value, value)
-}
-
-// Copying
-
-export function copy2 (original: Vector2): Vector2 {
-  return new VectorImpl(original.values.slice())
-}
-
-export function copy3 (original: Vector3): Vector3 {
-  return new VectorImpl(original.values.slice())
-}
-
-export function copy4 (original: Vector4): Vector4 {
-  return new VectorImpl(original.values.slice())
-}
-
-// Properties
-
-export function sizeSquared (vector: Vector): number {
-  let sum = 0
-  for (const elem of vector.values) {
-    sum += Math.pow(elem, 2)
+  /**
+   * Creates a copy of the vector.
+   */
+  clone (): Vector3 {
+    return new Vector3(this.x, this.y, this.z)
   }
 
-  return sum
-}
-
-export function size (vector: Vector): number {
-  return Math.sqrt(sizeSquared(vector))
-}
-
-export function sum (vector: Vector): number {
-  let sum = 0
-  for (const elem of vector.values) {
-    sum += elem
+  /**
+   * Returns squared vector size (squared length).
+   */
+  sizeSquared (): number {
+    return this.x ** 2 + this.y ** 2 + this.z ** 2
   }
-  return sum
-}
 
-// Normalization
-
-export function normalize2 (vector: Vector2): Vector2 {
-  return divide2(vector, num2(size(vector)))
-}
-
-export function normalize3 (vector: Vector3): Vector3 {
-  return divide3(vector, num3(size(vector)))
-}
-
-export function normalize4 (vector: Vector4): Vector4 {
-  return divide4(vector, num4(size(vector)))
-}
-
-// Multiplication
-
-export function multiply2 (left: Vector2, right: Vector2): Vector2 {
-  return vec2(left.x * right.x, left.y * right.y)
-}
-
-export function multiply3 (left: Vector3, right: Vector3): Vector3 {
-  return vec3(left.x * right.x, left.y * right.y, left.z * right.z)
-}
-
-export function multiply4 (left: Vector4, right: Vector4): Vector4 {
-  return vec4(left.x * right.x, left.y * right.y, left.z * right.z, left.w * right.w)
-}
-
-// Division
-
-export function divide2 (left: Vector2, right: Vector2): Vector2 {
-  return vec2(left.x / right.x, left.y / right.y)
-}
-
-export function divide3 (left: Vector3, right: Vector3): Vector3 {
-  return vec3(left.x / right.x, left.x / right.y, left.z / right.z)
-}
-
-export function divide4 (left: Vector4, right: Vector4): Vector4 {
-  return vec4(left.x / right.x, left.y / right.y, left.z / right.z, left.w / right.w)
-}
-
-// Addition
-
-export function add2 (left: Vector2, right: Vector2): Vector2 {
-  return vec2(left.x + right.x, left.y + right.y)
-}
-
-export function add3 (left: Vector3, right: Vector3): Vector3 {
-  return vec3(left.x + right.x, left.x + right.y, left.z + right.z)
-}
-
-export function add4 (left: Vector4, right: Vector4): Vector4 {
-  return vec4(left.x + right.x, left.y + right.y, left.z + right.z, left.w + right.w)
-}
-
-// Subtraction
-
-export function subtract2 (left: Vector2, right: Vector2): Vector2 {
-  return vec2(left.x - right.x, left.y - right.y)
-}
-
-export function subtract3 (left: Vector3, right: Vector3): Vector3 {
-  return vec3(left.x - right.x, left.x - right.y, left.z - right.z)
-}
-
-export function subtract4 (left: Vector4, right: Vector4): Vector4 {
-  return vec4(left.x - right.x, left.y - right.y, left.z - right.z, left.w - right.w)
-}
-
-// Dot product
-
-export function dot<T extends Vector> (left: T, right: T): number {
-  let sum = 0
-  for (let i = 0; i < left.values.length; i++) {
-    sum += (left.values[i] ?? 0) * (right.values[i] ?? 0)
+  /**
+   * Returns vector size (length).
+   */
+  size (): number {
+    return Math.sqrt(this.sizeSquared())
   }
-  return sum
+
+  /**
+   * Returns a normalized copy of the vector.
+   */
+  normalize (): Vector3 {
+    const size = this.size()
+    return new Vector3(this.x / size, this.y / size, this.z / size)
+  }
+
+  /**
+   * Returns a sum of vector components.
+   */
+  sum (): number {
+    return this.x + this.y + this.z
+  }
+
+  /**
+   * Returns a new vector representing a sum of two vectors.
+   * @param v Vector to add.
+   */
+  add (v: Vector3): Vector3 {
+    return new Vector3(this.x + v.x, this.y + v.y, this.z + v.z)
+  }
+
+  /**
+   * Returns a new vector representing a difference between two vectors.
+   * @param v Vector to subtract.
+   */
+  subtract (v: Vector3): Vector3 {
+    return new Vector3(this.x - v.x, this.y - v.y, this.z - v.z)
+  }
+
+  /**
+   * Returns a new vector where each component is a product of
+   * corresponding components in original vectors.
+   * @param v Second vector.
+   */
+  multiply (v: Vector3): Vector3 {
+    return new Vector3(this.x * v.x, this.y * v.y, this.z * v.z)
+  }
+
+  /**
+   * Returns a new vector where each component is a ratio of
+   * corresponding components in original vectors.
+   * @param v Second vector.
+   */
+  divide (v: Vector3): Vector3 {
+    return new Vector3(this.x / v.x, this.y / v.y, this.z / v.z)
+  }
+
+  /**
+   * Returns a dot product of two vectors.
+   * @param v Second vector.
+   */
+  dot (v: Vector3): number {
+    return this.x * v.x + this.y * v.y + this.z * v.z
+  }
+
+  /**
+   * Returns a cross product of two vectors.
+   * @param v Second vector.
+   */
+  cross (v: Vector3): Vector3 {
+    return new Vector3(
+      this.y * v.z - this.z * v.y,
+      this.z * v.x - this.x * v.z,
+      this.x * v.y - this.y * v.x
+    )
+  }
+}
+
+/**
+ * 4D vector.
+ */
+export class Vector4 {
+  public constructor (public x: number, public y: number, public z: number, public w: number) {}
+
+  /**
+   * Creates a copy of the vector.
+   */
+  clone (): Vector4 {
+    return new Vector4(this.x, this.y, this.z, this.w)
+  }
+
+  /**
+   * Returns squared vector size (squared length).
+   */
+  sizeSquared (): number {
+    return this.x ** 2 + this.y ** 2 + this.z ** 2 + this.w ** 2
+  }
+
+  /**
+   * Returns vector size (length).
+   */
+  size (): number {
+    return Math.sqrt(this.sizeSquared())
+  }
+
+  /**
+   * Returns a normalized copy of the vector.
+   */
+  normalize (): Vector4 {
+    const size = this.size()
+    return new Vector4(this.x / size, this.y / size, this.z / size, this.w / size)
+  }
+
+  /**
+   * Returns a sum of vector components.
+   */
+  sum (): number {
+    return this.x + this.y + this.z + this.w
+  }
+
+  /**
+   * Returns a new vector representing a sum of two vectors.
+   * @param v Vector to add.
+   */
+  add (v: Vector4): Vector4 {
+    return new Vector4(this.x + v.x, this.y + v.y, this.z + v.z, this.w + v.w)
+  }
+
+  /**
+   * Returns a new vector representing a difference between two vectors.
+   * @param v Vector to subtract.
+   */
+  subtract (v: Vector4): Vector4 {
+    return new Vector4(this.x - v.x, this.y - v.y, this.z - v.z, this.w - v.w)
+  }
+
+  /**
+   * Returns a new vector where each component is a product of
+   * corresponding components in original vectors.
+   * @param v Second vector.
+   */
+  multiply (v: Vector4): Vector4 {
+    return new Vector4(this.x * v.x, this.y * v.y, this.z * v.z, this.w * v.w)
+  }
+
+  /**
+   * Returns a new vector where each component is a ratio of
+   * corresponding components in original vectors.
+   * @param v Second vector.
+   */
+  divide (v: Vector4): Vector4 {
+    return new Vector4(this.x / v.x, this.y / v.y, this.z / v.z, this.w / v.w)
+  }
+
+  /**
+   * Returns a dot product of two vectors.
+   * @param v Second vector.
+   */
+  dot (v: Vector4): number {
+    return this.x * v.x + this.y * v.y + this.z * v.z + this.w * v.w
+  }
 }
