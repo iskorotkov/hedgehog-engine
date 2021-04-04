ee4066186eb5bfaaceb7c1c03390035240c97e54
Ivan <korotkov.ivan.s@gmail.com>
2021-04-05 00:12:07 +0500

    feat: calculate normals for revolution body

--- src/special/revolutionBody.ts
+++ src/special/revolutionBody.ts
@@ -22,39 +22,82 @@ function pointsOnShape (points: Vector2[], axis: Vector3, segments: number) {
   return shapePoints
 }
 
-export function revolutionBody (points: Vector2[], axis: Vector3, segments: number, cap: boolean) {
+function calculateUV (nPoints: number, nSegments: number, point: number, segment: number, cap: boolean): Vector2 {
+  if (cap) {
+    return new Vector2(segment / nSegments, (point + 1) / (nPoints + 1))
+  } else {
+    return new Vector2(segment / nSegments, point / (nPoints - 1))
+  }
+}
+
+function calculateNormals (points: Vector3[], nSegments: number, point: number, segment: number): Vector3 {
+  const nPoints = points.length / (nSegments + 1)
+
+  // TODO: Use top vertex used for capping.
+  const findTopVertex = () => point > 0
+    ? points[segment * nPoints + point - 1]
+    : points[segment * nPoints + point]
+
+  // TODO: Use bottom vertex used for capping.
+  const findBottomVertex = () => point < nPoints - 1
+    ? points[segment * nPoints + point + 1]
+    : points[segment * nPoints + point]
+
+  const findLeftVertex = () => segment > 0
+    ? points[(segment - 1) * nPoints + point]
+    : points[(nSegments - 2) * nPoints + point]
+
+  const findRightVertex = () => segment < nSegments - 1
+    ? points[(segment + 1) * nPoints + point]
+    : points[point]
+
+  const top = findTopVertex()
+  const bottom = findBottomVertex()
+  const left = findLeftVertex()
+  const right = findRightVertex()
+
+  if (!top || !bottom || !left || !right) throw Error()
+
+  const topToBottom = bottom.subtract(top).normalize()
+  const leftToRight = right.subtract(left).normalize()
+
+  const normal = topToBottom.cross(leftToRight)
+  return normal
+}
+
+export function revolutionBody (points: Vector2[], axis: Vector3, nSegments: number, cap: boolean) {
   if (points.length < 4) {
     return new SimpleModel([], [])
   }
 
   axis = axis.normalize()
 
-  const shape = pointsOnShape(points, axis, segments)
+  const shape = pointsOnShape(points, axis, nSegments)
 
   const vertices = []
   const indices = []
 
   // Add vertices.
-  for (let segment = 0; segment <= segments; segment++) {
+  for (let segment = 0; segment <= nSegments; segment++) {
     for (let pt = 0; pt < points.length; pt++) {
       const point = shape[segment * points.length + pt]
       if (!point) {
         throw Error()
       }
 
-      // TODO: Calculate normals (cross product).
-      // TODO: Calculate UV coordinates. Check for correctness.
+      const uv = calculateUV(points.length, nSegments, pt, segment, cap)
+      const normal = calculateNormals(shape, nSegments, pt, segment)
 
-      if (cap) {
-        vertices.push(point.x, point.y, point.z, point.x, point.y, point.z, segment / segments, (pt + 1) / (points.length + 1))
-      } else {
-        vertices.push(point.x, point.y, point.z, point.x, point.y, point.z, segment / segments, pt / (points.length - 1))
-      }
+      vertices.push(
+        point.x, point.y, point.z,
+        normal.x, normal.y, normal.z,
+        uv.x, uv.y
+      )
     }
   }
 
   // Add indices.
-  for (let segment = 0; segment < segments; segment++) {
+  for (let segment = 0; segment < nSegments; segment++) {
     for (let pt = 0; pt < points.length - 1; pt++) {
       const v0 = segment * points.length + pt
       const v1 = segment * points.length + pt + 1
@@ -90,24 +133,24 @@ export function revolutionBody (points: Vector2[], axis: Vector3, segments: numb
 
     // Top triangles.
     vertices.push(topVertex.x, topVertex.y, topVertex.z, normal.x, normal.y, normal.z, 0, 0)
-    for (let segment = 0; segment < segments; segment++) {
+    for (let segment = 0; segment < nSegments; segment++) {
       const v0 = segment * points.length
       const v1 = (segment + 1) * points.length
 
       // Top vertex.
-      const v2 = (segments + 1) * points.length
+      const v2 = (nSegments + 1) * points.length
 
       indices.push(v0, v1, v2)
     }
 
     // Bottom trianges.
     vertices.push(bottomVertex.x, bottomVertex.y, bottomVertex.z, -normal.x, -normal.y, -normal.z, 0, 1)
-    for (let segment = 0; segment < segments; segment++) {
+    for (let segment = 0; segment < nSegments; segment++) {
       const v0 = (segment + 1) * points.length - 1
       const v1 = (segment + 2) * points.length - 1
 
       // Bottom vertex.
-      const v2 = (segments + 1) * points.length + 1
+      const v2 = (nSegments + 1) * points.length + 1
 
       indices.push(v0, v1, v2)
     }
